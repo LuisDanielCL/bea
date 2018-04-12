d1a4a25628ee32eebe69bf0311c27d783772c0af
MiguelJack <miguelfenix16@gmail.com>
2018-04-11 18:50:00 -0600

    registro
    registro completo

--- pages/js/agregarTecnico.js
+++ pages/js/agregarTecnico.js
@@ -1,42 +1,21 @@
 function agregarTecnico(){
-    if (document.getElementById('txtNombre').value == "") {
-        alert('El nombre es requerido');
-    }else{ 
-        if (document.getElementById('txtApellidoUno').value == "") {
-            alert('El primer apellido es requerido');
-        }else{
-            if (document.getElementById('txtApellidoDos').value == "") {
-                alert('El segundo apellido es requerido');
-            }else{
-                if (document.getElementById('txtUser').value == "") {
-                    alert('El usuario es requerido');
-                }else{
-                    if (document.getElementById('txtPass').value == "") {
-                        alert('El password es requerido');
-                    }else{
-                        var parametros = {
-                            opcion : "agregarUsuario",
-                            txtNombre: $('#txtNombre').val(),
-                            txtApellidoUno: $('#txtApellidoUno').val(),
-                            txtApellidoDos: $('#txtApellidoDos').val(),
-                            txtUser: $('#txtUser').val(),
-                            txtPass: $('#txtPass').val(),
-                            txtNombre: $('#txtNombre').val(),
-                            txtTelefono: $('#txtTelefono').val(),
-                            txtCorreo: $('#txtCorreo').val()
-                        };
+    var parametros = {
+        opcion : "agregarUsuario",
+        txtNombre: $('#txtNombre').val(),
+        txtApellidoUno: $('#txtApellidoUno').val(),
+        txtApellidoDos: $('#txtApellidoDos').val(),
+        txtUser: $('#txtUser').val(),
+        txtPass: $('#txtPass').val(),
+        txtTelefono: $('#txtTelefono').val(),
+        txtCorreo: $('#txtCorreo').val()
+    };
 
-                        // Realizar la petición
-                        var post = $.post(
-                                              "php/mysql.php",    // Script que se ejecuta en el servidor
-                                              parametros,                              
-                                              siRespuesta    // Función que se ejecuta cuando el servidor responde
-                                              );
-                    }
-                }
-            }
-        }
-    }
+    // Realizar la petición
+    var post = $.post(
+                          "php/mysql.php",    // Script que se ejecuta en el servidor
+                          parametros,                              
+                          siRespuesta    // Función que se ejecuta cuando el servidor responde
+                          );
 }
 
 function siRespuesta(r){
@@ -53,3 +32,8 @@ function limpiar(){
     document.getElementById('txtTelefono').value = "";
     document.getElementById('txtCorreo').value = "";
 }
+
+$( "#tecnicoForm" ).submit(function( event ) {
+    agregarTecnico();
+    return false;
+});
